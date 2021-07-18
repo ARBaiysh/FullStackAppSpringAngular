2d83b634f1329997695f186159422957a06c0fd9
Workstation <o0052ib@gmail.com>
2021-07-18 15:23:11 +0600

    forward

---
+++ src/main/java/kg/baiysh/FullStackAppJavaSpringAndAngular/FullStackAppJavaSpringAndAngularApplication.java
@@ -0,0 +1,13 @@
+package kg.baiysh.FullStackAppJavaSpringAndAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAppJavaSpringAndAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAppJavaSpringAndAngularApplication.class, args);
+	}
+
+}
