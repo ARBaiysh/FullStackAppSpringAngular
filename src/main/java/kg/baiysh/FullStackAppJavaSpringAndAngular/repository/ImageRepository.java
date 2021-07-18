8451703e1c0f1abfff1cf586278664d997ae1954
Workstation <o0052ib@gmail.com>
2021-07-18 15:45:57 +0600

    add Repository

---
+++ src/main/java/kg/baiysh/FullStackAppJavaSpringAndAngular/repository/ImageRepository.java
@@ -0,0 +1,17 @@
+package kg.baiysh.FullStackAppJavaSpringAndAngular.repository;
+
+
+import kg.baiysh.FullStackAppJavaSpringAndAngular.entity.ImageModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ImageRepository extends JpaRepository<ImageModel, Long> {
+
+    Optional<ImageModel> findByUserId(Long userId);
+
+    Optional<ImageModel> findByPostId(Long postId);
+
+}
